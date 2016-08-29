c89744cd9302e4c23728af718f07829af8ed8133
sun <test@example.com>
2016-08-29 21:09:01 +0800

    eigth commit

--- fraction.java
+++ fraction.java
@@ -39,4 +39,16 @@ public class Fraction{
 	System.out.println(a+"/"+b);
 	}
  }
+	public static void main(String []args){
+	Scanner in =new Scanner(System.in);
+	Fraction a =new Fraction(in.nextInt(),in.nextInt());
+	Fraction b =new Fraction(in.nextInt(),in.nextInt());
+	a.print();
+	b.print();
+	a.puls(b).print();
+	a.multiply(b).plus(new Fraction(5,6)).print();
+	a.print();
+	b.print();
+	in.close();
+ }
 }
